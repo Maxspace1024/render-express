ce45a585c55fcd87b707313aeba1f0d081ea4d01
Maxspace1024 <jeb.3332@gmail.com>
2022-11-18 12:28:11 +0800

    rest api

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get("/json/:idx",(req, res)=>{
+	res.json("key":req.params.idx)
+})
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
